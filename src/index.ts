3cc6914ab7ef79ac5c0c917dbdf704850bb8c549
Joshua Sarmiento <supremojoshua22@gmail.com>
2026-06-03 16:49:44 +0800

    feat: export configureAnalyzer and cache adapters

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,19 @@ export {
   type TextAnalysisResult 
 } from './tesseractDetection';
 
+export {
+  configureAnalyzer,
+  type AnalyzerConfig
+} from './config';
+
+export {
+  type ICacheAdapter
+} from './utils/ICacheAdapter';
+
+export {
+  MemoryCacheAdapter
+} from './utils/cache';
+
 // Default export for convenience
 import { analyzeImageFast, analyzeImagesBatch } from './combinedImageAnalysis';
 import { terminateOCR } from './tesseractDetection';
